e3d3ec1e34a585c0fed63f5d6f71eda6ddc3c9d8
kenzhemir <miras.kenzhegaliyev@nu.edu.kz>
2018-03-01 15:31:27 +0600

    Add basic router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import 'App.css';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
 
 import LoginRegister from 'components/LoginRegister';
-import Header from "components/Header";
-import SideMenu from "components/SideMenu";
+//import Header from "components/Header";
+//import SideMenu from "components/SideMenu";
 import Landing from "scenes/Landing";
 import NotFound from "scenes/NotFound";
 import Dashboard from "scenes/Dashboard";
@@ -11,12 +12,14 @@ import Dashboard from "scenes/Dashboard";
 export default class App extends Component {
     render() {
         return (
-            <div className="App">
-                <Dashboard/>
-                <LoginRegister/>
-                <Landing/>
-                <NotFound/>
-            </div>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Landing}/>
+                    <Route exact path="/dashboard" component={Dashboard}/>
+                    <Route exact path="/login" component={LoginRegister}/>
+                    <Route component={NotFound}/>
+                </Switch>
+            </Router>
         );
     }
 }
